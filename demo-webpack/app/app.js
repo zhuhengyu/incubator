fdaeea6739b2e203df5a46a0290c97718035b194
CharlesOy <ch.k.ouyang@gmail.com>
2017-01-05 00:09:12 +0900

    remove demo-webpack

--- demo-webpack/app/app.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by ouyangcharles on 2016/12/26.
- */
-
-document.write(require('./test'));
--- demo-webpack/app/test.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by ouyangcharles on 2016/12/26.
- */
-
-module.exports = 'Hello, Real Webpack';
