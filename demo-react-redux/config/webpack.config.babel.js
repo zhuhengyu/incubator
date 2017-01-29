9b078b790a5ab2adf4d9ee7829cd88bf15dfdc79
CharlesOy <ch.k.ouyang@gmail.com>
2017-01-29 20:22:05 +0900

    webpack-bundle-analyzer

--- demo-react-redux/config/webpack.config.babel.js
+++ demo-react-redux/config/webpack.config.babel.js
@@ -7,6 +7,7 @@ import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import HappyPack from 'happypack';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 // const modules = {};
 // fs.readdirSync('node_modules')
@@ -79,9 +80,35 @@ const happyPackPlugin = new HappyPack({
     },
   }],
 });
+const bundleAnalyzerPlugin = new BundleAnalyzerPlugin({
+  // Can be `server`, `static` or `disabled`.
+  // In `server` mode analyzer will start HTTP server to show bundle report.
+  // In `static` mode single HTML file with bundle report will be generated.
+  // In `disabled` mode you can use this plugin to just generate Webpack Stats JSON file by setting `generateStatsFile` to `true`.
+  analyzerMode: 'server',
+  // Port that will be used in `server` mode to start HTTP server.
+  analyzerPort: 8888,
+  // Path to bundle report file that will be generated in `static` mode.
+  // Relative to bundles output directory.
+  reportFilename: 'report.html',
+  // Automatically open report in default browser
+  openAnalyzer: true,
+  // If `true`, Webpack Stats JSON file will be generated in bundles output directory
+  generateStatsFile: true,
+  // Name of Webpack Stats JSON file that will be generated if `generateStatsFile` is `true`.
+  // Relative to bundles output directory.
+  statsFilename: 'stats.json',
+  // Options for `stats.toJson()` method.
+  // For example you can exclude sources of your modules from stats file with `source: false` option.
+  // See more options here: https://github.com/webpack/webpack/blob/webpack-1/lib/Stats.js#L21
+  statsOptions: null,
+  // Log level. Can be 'info', 'warn', 'error' or 'silent'.
+  logLevel: 'info'
+});
 plugins.push(htmlWebpackPlugin);
 plugins.push(uglifyJsPlugin);
 plugins.push(productionPlugin);
+// plugins.push(bundleAnalyzerPlugin);
 // plugins.push(commonsChunkPlugin);
 // plugins.push(happyPackPlugin);
 
