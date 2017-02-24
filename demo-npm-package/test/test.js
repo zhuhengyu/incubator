95a251ce44eff807bc178251c5d9a9f98d3eb161
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-02-24 22:41:06 +0900

    demo-npm-package initialization

---
+++ demo-npm-package/test/test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by 欧阳超 on 2017/02/24
+ */
+
+const curry = require('../index').curry;
+
+const plus = curry((x, y, z) => x + y + z);
+
+console.log(plus(1, 2, 3));
+console.log(plus(1, 2)(3));
+console.log(plus(1)(2)(3));
+
