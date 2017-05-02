86ef7c687dd641d76a72b43f86cffd2685f27bfd
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-05-02 22:05:08 +0800

    test mongodb

---
+++ demo-spider/NetFictionAnalyser/TestMongoDb.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+
+# getting mongodb client
+client = MongoClient('localhost', 27017)
+# getting a database
+db = client.qidian
+# getting a collection
+collection = db.test
+
+
+def test():
+    print(collection.find_one())
+
+
+if __name__ == '__main__':
+    test()
