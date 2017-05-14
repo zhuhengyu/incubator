f695dee4c3443fd5043bd1242522d9d3a7ef8307
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-05-14 23:56:52 +0800

    convert json string to dict

--- demo-spider/NetFictionAnalyser/__init__.py
+++ demo-spider/NetFictionAnalyser/__init__.py
@@ -1,4 +1,5 @@
-from NetFictionAnalyser.QdPageHandler import QdPageHandler
+# from NetFictionAnalyser.QdPageHandler import QdPageHandler
+import json
 
 
 def test():
@@ -6,11 +7,17 @@ def test():
     test function
     :return:
     """
-    page_handler = QdPageHandler(order_id=5, style=2, sleep_range=0.5)
-    page_handler.set_page_range(start_page=1, end_page=-1)
-    page_handler.take_shortcut(if_print=True, output_file=False, from_when=120)
-    page_handler.handle(if_print=True)
+    # page_handler = QdPageHandler(order_id=5, style=2, sleep_range=0.5)
+    # page_handler.set_page_range(start_page=1, end_page=-1)
+    # page_handler.take_shortcut(if_print=True, output_file=False, from_when=120)
+    # page_handler.handle(if_print=True)
     # page_handler.to_db()
+    temp = "{'id': '1008865464', 'retrieveTime': 1494776994.711978, 'bookName': '三国逸士', 'author': '流风逐云', " \
+           "'tags': ['连载', '免费', '历史', '秦汉三国'], 'charCountNow': 15700, 'totalClicks': 39, 'vipWeekClicks': 7, " \
+           "'totalRecommendations': 1, 'vipWeekRecommendations': 1, 'category': '历史频道', 'subCategory': '秦汉三国', " \
+           "'updateTime': 1494773375.0, 'selfDefinedTags': [], 'authorNovelCount': 1, 'authorTotalCharCount': 15700, " \
+           "'authorProducingDays': 8}"
+    print(json.loads(temp.replace("'", '"')))
 
 
 if __name__ == '__main__':
