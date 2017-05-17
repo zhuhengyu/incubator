a9fc9132599b1bb541064a9b1fb92fbab37b9b1e
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-05-17 19:29:06 +0800

    elegant pie chart now

--- demo-spider/NetFictionAnalyser/StatisticsHandler.py
+++ demo-spider/NetFictionAnalyser/StatisticsHandler.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import datetime
 
 from NetFictionAnalyser.Conns import conn_db
@@ -13,8 +14,10 @@ def cat_pie(fictions_statistics):
     labels = list(fictions_statistics.keys())
     sizes = list(fictions_statistics.values())
     explode = [0 for i in range(0, len(labels))]
+
     fig1, ax1 = plt.subplots()
-    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=False, startangle=90)
+    ax1.pie(sizes, explode=explode, labels=labels, colors=get_color_map(14), autopct='%1.1f%%', shadow=False,
+            startangle=90)
     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
     plt.show()
 
@@ -28,10 +31,23 @@ def statistics_cat(from_when=86400):
     from_when = now - from_when
     result = {}
     fictions = conn_db().find({'updateTime': {'$gt': from_when}})
-    for index, fiction in enumerate(fictions):
-        print(index)
+    for fiction in fictions:
         if fiction['category'] not in result:
             result[fiction['category']] = 1
         else:
             result[fiction['category']] += 1
     return result
+
+
+def get_color_map(n):
+    """
+    Returns a function that maps each index in 0, 1, ... N-1 to a distinct
+    RGB color.
+    :param n:
+    :return:
+    """
+    # color map
+    # jet viridis
+    color_map = plt.get_cmap('viridis')
+    colors = color_map(np.linspace(0, 1, n))
+    return colors
