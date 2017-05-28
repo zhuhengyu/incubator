4e701e7221617aebc7e0e5b1b4eccff9e82cdf22
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-05-29 00:14:32 +0800

    vue $emit

--- demo-vue/javascripts/components.js
+++ demo-vue/javascripts/components.js
@@ -31,5 +31,45 @@ Vue.component('input-todo', {
 		content() {
 			console.log(this.content);
 		}
+	},
+	methods: {
+		addNewTodo() {
+			this.$emit('addnewtodo', {
+				id: Math.floor(Math.random() * 1e10),
+				title: this.title,
+				content: this.content
+			});
+			this.title = '';
+			this.content = '';
+		}
+	}
+});
+
+
+const app = new Vue({
+	el: '#app',
+	data: {
+		todos: [{
+			id: Math.floor(Math.random() * 1e10),
+			title: 'todo1',
+			content: 'eat breakfast'
+		},{
+			id: Math.floor(Math.random() * 1e10),
+			title: 'todo2',
+			content: 'eat lunch'
+		},{
+			id: Math.floor(Math.random() * 1e10),
+			title: 'todo3',
+			content: 'eat supper'
+		},{
+			id: Math.floor(Math.random() * 1e10),
+			title: 'todo4',
+			content: 'drink milk'
+		}]
+	},
+	methods: {
+		addNewTodo(todo) {
+			this.todos.push(todo);
+		}
 	}
 });
