9d762e6381b26ab6417aae82866181316376eb9c
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-05-29 10:34:43 +0800

    multiple todolist

--- demo-vue/javascripts/components.js
+++ demo-vue/javascripts/components.js
@@ -1,5 +1,5 @@
 Vue.component('todo-item', {
-	props: ['todo'],
+	props: ['listId', 'todo'],
 	template: '#todo-item',
 	data() {
 		return {
@@ -14,13 +14,14 @@ Vue.component('todo-item', {
 			alert(this.todo.content);
 		},
 		removeTodo() {
-			this.$parent.$emit('removetodo', this.todo.id);
+			this.$parent.$emit('removetodo', this.listId, this.todo.id);
 		}
 	}
 });
 
 Vue.component('input-todo', {
 	template: '#input-todo',
+	props: ['listId'],
 	data() {
 		return {
 			title: '',
@@ -38,9 +39,12 @@ Vue.component('input-todo', {
 	methods: {
 		addNewTodo() {
 			this.$parent.$emit('addnewtodo', {
-				id: Math.floor(Math.random() * 1e10),
-				title: this.title,
-				content: this.content
+				listId: this.listId,
+				todo: {
+					id: genId(),
+					title: this.title,
+					content: this.content
+				}
 			});
 			this.title = '';
 			this.content = '';
@@ -57,35 +61,68 @@ Vue.component('todo-list', {
 const app = new Vue({
 	el: '#app',
 	data: {
-		todoList: {
+		todoLists: [{
+			id: genId(),
 			title: 'Zhao',
 			todos: [{
-				id: Math.floor(Math.random() * 1e10),
+				id: genId(),
 				title: 'todo1',
 				content: 'eat breakfast'
 			},{
-				id: Math.floor(Math.random() * 1e10),
+				id: genId(),
 				title: 'todo2',
 				content: 'eat lunch'
 			},{
-				id: Math.floor(Math.random() * 1e10),
+				id: genId(),
 				title: 'todo3',
 				content: 'eat supper'
 			},{
-				id: Math.floor(Math.random() * 1e10),
+				id: genId(),
 				title: 'todo4',
 				content: 'drink milk'
 			}]
-		}
+		},{
+			id: genId(),
+			title: 'Qian',
+			todos: [{
+				id: genId(),
+				title: 'todo1',
+				content: 'eat breakfast'
+			},{
+				id: genId(),
+				title: 'todo2',
+				content: 'eat lunch'
+			},{
+				id: genId(),
+				title: 'todo3',
+				content: 'eat supper'
+			},{
+				id: genId(),
+				title: 'todo4',
+				content: 'drink milk'
+			}]
+		}]
 	},
 	methods: {
-		addNewTodo(todo) {
-			this.todos.push(todo);
+		addNewTodo(data) {
+			this.todoLists.forEach(function(todoList) {
+				if (todoList.id === data.listId) {
+					todoList.todos.push(data.todo);
+				}
+			});
 		},
-		removeTodo(id) {
-			this.todos = this.todos.filter(function(todo) {
-				return todo.id !== id;
+		removeTodo(listId, todoId) {
+			this.todoLists.forEach(function(todoList) {
+				if(todoList.id === listId) {
+					todoList.todos = todoList.todos.filter(function(todo) {
+						return todo.id !== todoId;
+					});
+				}
 			});
 		}
 	}
 });
+
+function genId() {
+	return Math.floor(Math.random() * 1e10);
+}
