3856487ce7b15e98d0ae3865478cdf0cd8e89ec5
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-06-05 18:24:42 +0800

    design pattern 02 polymorphism

---
+++ design-pattern/02-2-polymorphism.js
@@ -0,0 +1,24 @@
+function Duck() {}
+function Chicken() {}
+
+Duck.prototype.sound = function() {
+	console.log('gagaga');
+};
+
+Chicken.prototype.sound = function() {
+	console.log('gegege');
+};
+
+function makeSound(animal) {
+	animal.sound();
+}
+
+makeSound(new Duck());
+makeSound(new Chicken());
+
+function Dog() {}
+Dog.prototype.sound = function() {
+	console.log('wangwang');
+};
+
+makeSound(new Dog());
