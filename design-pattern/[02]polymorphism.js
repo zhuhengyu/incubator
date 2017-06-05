226e8e167eb49e569bce11b60b4b769d67aaf513
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-06-05 11:45:07 +0800

    design pattern 02 polymorphism

---
+++ design-pattern/[02]polymorphism.js
@@ -0,0 +1,13 @@
+function Duck() {}
+function Chicken() {}
+
+function makeSound(animal) {
+	if (animal instanceof Duck) {
+		console.log('gagaga');
+	} else if(animal instanceof Chicken) {
+		console.log('gegege');
+	}
+}
+
+makeSound(new Duck());
+makeSound(new Chicken());
