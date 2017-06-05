dbb5e11db81cb96fc9420b69e37ef716543404d0
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-06-05 11:41:02 +0800

    design pattern 01 duck type

---
+++ design-pattern/[01]duck-type.js
@@ -0,0 +1,26 @@
+let duck = {
+	duckSinging() {
+		console.log('gagaga');
+	}
+};
+
+let chicken = {
+	duckSinging() {
+		console.log('gagaga');
+	}
+};
+
+let choir = [];
+
+let joinChoir = function(animal) {
+	if (animal && typeof animal.duckSinging === 'function') {
+		choir.push(animal);
+	}
+}
+
+joinChoir(duck);
+joinChoir(chicken);
+
+for(let animal of choir) {
+	animal.duckSinging();
+}
