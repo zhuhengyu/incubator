d94df87da0c8c358e84603e8497ba7ddc122de82
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-06-07 15:57:03 +0800

    design pattern singleton

---
+++ design-pattern/03-1-singleton.js
@@ -0,0 +1,21 @@
+function Singleton(name) {
+	this.name = name;
+}
+
+Singleton.prototype.getName = function() {
+	return this.name;
+};
+
+Singleton.getInstance = function(name) {
+	if(!this.instance) {
+		this.instance = new Singleton(name);
+	}
+	return this.instance;
+};
+
+const foo = Singleton.getInstance('foo');
+const bar = Singleton.getInstance('bar');
+
+console.log(foo.getName());
+console.log(bar.getName());
+console.log(foo === bar);
