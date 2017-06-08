da3814e29574184b69e2f8fd559bf6bce87d6243
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-06-08 12:03:03 +0800

    design pattern strategy

---
+++ design-pattern/04-1-strategy.js
@@ -0,0 +1,11 @@
+const strategy = {
+	S: (salary)=> salary * 4,
+	A: (salary)=> salary * 3,
+	B: (salary)=> salary * 2,
+};
+
+const calculateBonus = (strategyKey, salary) => strategy[strategyKey](salary);
+
+console.log(calculateBonus('S', 10000));
+console.log(calculateBonus('A', 10000));
+console.log(calculateBonus('B', 10000));
