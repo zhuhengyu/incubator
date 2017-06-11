6249e0cfb711369bb38046ef1155d2027f5f8180
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-06-11 21:49:41 +0800

    design pattern proxy

--- design-pattern/05-2-proxy.js
+++ design-pattern/05-2-proxy.js
@@ -7,19 +7,38 @@ const multi = function() {
 	return result;
 };
 
-const multiProxy = (function() {
-	const cache = {};
-	return function() {
-		const args = Array.from(arguments);
-		const key = args.join(',');
-		if (key in cache) {
-			return cache[key];
-		}
-		return cache[key] = multi.apply(null, args);
-	};
-})();
+const plus = function() {
+	const args = Array.from(arguments);
+	let result = 0;
+	for (var i = 0; i < args.length; i++) {
+		result += args[i];
+	}
+	return result;
+};
+
+const createProxyFac = function(fn) {
+	return (function(fn) {
+		const cache = {};
+		return function() {
+			const args = Array.from(arguments);
+			const key = args.join(',');
+			if (key in cache) {
+				return cache[key];
+			}
+			return cache[key] = fn.apply(null, args);
+		};
+	})(fn);
+};
+
+const multiProxy = createProxyFac(multi);
+const plusProxy = createProxyFac(plus);
 
 console.log(multiProxy(1, 2, 3, 4));
 console.log(multiProxy(1, 2, 3, 4));
 console.log(multiProxy(3, 4));
 console.log(multiProxy(3, 4));
+
+console.log(plusProxy(1, 2, 3, 4));
+console.log(plusProxy(1, 2, 3, 4));
+console.log(plusProxy(3, 4));
+console.log(plusProxy(3, 4));
