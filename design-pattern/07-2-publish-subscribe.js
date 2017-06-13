95798e20a13b4d8f9b7ec5bc4f9110fe28ba24e6
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-06-14 00:04:02 +0800

    design pattern publish subscribe

--- design-pattern/07-2-publish-subscribe.js
+++ design-pattern/07-2-publish-subscribe.js
@@ -9,6 +9,17 @@ const installEvent = function(obj) {
 			}
 			this.clientLists[key].push(callback);
 		},
+		remove(key, callback) {
+			const clientList = this.clientLists[key];
+			if (!clientList) {
+				return;
+			}
+			for (var i = clientList.length - 1; i >= 0; i--) {
+				if (clientList[i] === callback) {
+					clientList.splice(i, 1);
+				}
+			}
+		},
 		trigger() {
 			const key = Array.prototype.shift.apply(arguments);
 			const clientList = this.clientLists[key];
@@ -29,17 +40,22 @@ const installEvent = function(obj) {
 
 installEvent(publisher);
 
-publisher.listen('e1', function(arg) {
-	console.log(`hahaha ${arg}`);
-});
-publisher.listen('e1', function(arg) {
-	console.log(`hehehe ${arg}`);
-});
-publisher.listen('e1', function(arg) {
-	console.log(`hihihi ${arg}`);
-});
+const xiaoming = function(arg) {
+	console.log(`xiaoming ${arg}`);
+};
+
+const xiaofang = function(arg) {
+	console.log(`xiaofang ${arg}`);
+};
+
+publisher.listen('e1', xiaoming);
+publisher.listen('e1', xiaofang);
 publisher.listen('e2', function(arg) {
-	console.log(`hahaha ${arg}`);
+	console.log(`xiaoma ${arg}`);
 });
 
 publisher.trigger('e1', 200);
+
+publisher.remove('e1', xiaoming);
+
+publisher.trigger('e1', 300);
