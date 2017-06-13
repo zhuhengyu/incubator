2da2588c8738e440f63df7d3c6ed207fb348dce7
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-06-13 23:45:25 +0800

    design pattern publish subscribe

--- design-pattern/07-2-publish-subscribe.js
+++ design-pattern/07-2-publish-subscribe.js
@@ -1,32 +1,33 @@
 const publisher = {};
 
-const event = {
-	clientLists: {},
-	listen(key, callback) {
-		if (!this.clientLists[key]) {
-			this.clientLists[key] = [];
+const installEvent = (function() {
+	const event = {
+		clientLists: {},
+		listen(key, callback) {
+			if (!this.clientLists[key]) {
+				this.clientLists[key] = [];
+			}
+			this.clientLists[key].push(callback);
+		},
+		trigger() {
+			const key = Array.prototype.shift.apply(arguments);
+			const clientList = this.clientLists[key];
+			if (!clientList) {
+				return;
+			}
+			for (var i = 0; i < clientList.length; i++) {
+				clientList[i].apply(this, arguments);
+			}
 		}
-		this.clientLists[key].push(callback);
-	},
-	trigger() {
-		const key = Array.prototype.shift.apply(arguments);
-		const clientList = this.clientLists[key];
-		if (!clientList) {
-			return;
+	};
+	return function(obj) {
+		if (obj) {
+			for(let key in event) {
+				obj[key] = event[key];
+			};
 		}
-		for (var i = 0; i < clientList.length; i++) {
-			clientList[i].apply(this, arguments);
-		}
-	}
-};
-
-const installEvent = function(obj) {
-	if (obj) {
-		for(let key in event) {
-			obj[key] = event[key];
-		};
-	}
-};
+	};
+})();
 
 installEvent(publisher);
 
