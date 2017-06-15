40ffd09c5d2cd5c24ed12c02ff376b81bd783cd5
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-06-15 22:18:49 +0800

    design pattern publish subscribe

--- design-pattern/07-2-publish-subscribe.js
+++ design-pattern/07-2-publish-subscribe.js
@@ -1,57 +1,53 @@
-const publisher = {};
-
 const installEvent = function(obj) {
-	const event = {
-		clientLists: {},
-		listen(key, callback) {
-			if (!this.clientLists[key]) {
-				this.clientLists[key] = [];
-			}
-			this.clientLists[key].push(callback);
-		},
-		remove(key, callback) {
-			const clientList = this.clientLists[key];
-			if (!clientList) {
-				return;
-			}
-			for (var i = clientList.length - 1; i >= 0; i--) {
-				if (clientList[i] === callback) {
-					clientList.splice(i, 1);
-				}
-			}
-		},
-		trigger() {
-			const key = Array.prototype.shift.apply(arguments);
-			const clientList = this.clientLists[key];
-			if (!clientList) {
-				return;
-			}
-			for (var i = 0; i < clientList.length; i++) {
-				clientList[i].apply(this, arguments);
-			}
-		}
-	};
-	if (obj) {
-		for(let key in event) {
-			obj[key] = event[key];
-		};
-	}
-};
+  const event = {
+    clients: {},
+    listen(e, fn) {
+      if (!this.clients[e]) {
+        this.clients[e] = [];
+      }
+      this.clients[e].push(fn);
+    },
+    trigger() {
+      const e = Array.prototype.shift.call(arguments);
+      if (this.clients[e]) {
+        for(let fn of this.clients[e]) {
+          fn.apply(this, arguments);
+        }
+      }
+    },
+    remove(e, fn) {
+      if (this.clients[e]) {
+        for (var i = this.clients[e].length - 1; i >= 0; i--) {
+          if (this.clients[e][i] === fn) {
+            this.clients[e].splice(i, 1);
+          }
+        }
+      }
+    }
+  };
+  if (obj) {
+    for(let k in event) {
+      obj[k] = event[k];
+    }
+  }
+}
+
+const publisher = {};
 
 installEvent(publisher);
 
 const xiaoming = function(arg) {
-	console.log(`xiaoming ${arg}`);
+  console.log(`xiaoming ${arg}`);
 };
 
 const xiaofang = function(arg) {
-	console.log(`xiaofang ${arg}`);
+  console.log(`xiaofang ${arg}`);
 };
 
 publisher.listen('e1', xiaoming);
 publisher.listen('e1', xiaofang);
 publisher.listen('e2', function(arg) {
-	console.log(`xiaoma ${arg}`);
+  console.log(`xiaoma ${arg}`);
 });
 
 publisher.trigger('e1', 200);
