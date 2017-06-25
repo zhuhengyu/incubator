3ac8d5fcecfc4a4dc2e96eb8f57e70006ebec850
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-06-26 00:04:28 +0800

    design pattern template

--- design-pattern/09-5-template.js
+++ design-pattern/09-5-template.js
@@ -1,26 +1,36 @@
-const Person = function() {};
+const Person = function(name) {
+  this.name = name;
+};
 Person.prototype.openDoor = function() {
-  console.log('Open door.');
+  console.log(`${this.name} is opening door.`);
 };
 Person.prototype.doSth = function() {};
+Person.prototype.haveSupper = function() {
+  console.log(`${this.name} having supper`);
+};
 Person.prototype.goHome = function() {
   this.openDoor();
   this.doSth();
+  this.haveSupper();
 };
 
-const Adult = function() {};
+const Adult = function(name) {
+  Person.call(this, name);
+};
 Adult.prototype = new Person();
 Adult.prototype.doSth = function() {
-  console.log('Watch TV.');
+  console.log(`${this.name} watching TV.`);
 };
 
-const Child = function() {};
+const Child = function(name) {
+  Person.call(this, name);
+};
 Child.prototype = new Person();
 Child.prototype.doSth = function() {
-  console.log('Do homework.');
+  console.log(`${this.name} is doing homework.`);
 };
 
-const p1 = new Adult();
+const p1 = new Adult('Zhao');
 p1.goHome();
-const p2 = new Child();
+const p2 = new Child('Qian');
 p2.goHome();
