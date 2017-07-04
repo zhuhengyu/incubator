0e402e9e5834257f3e59b6ae997886496633df5d
Ouyang <ch.k.ouyang@gmail.com>
2017-07-04 20:30:33 +0800

    jdash bug fixes & appendTo & remove

--- demo-jquery/jdash.js
+++ demo-jquery/jdash.js
@@ -1,26 +1,36 @@
 (function(freeSelf) {
+
+  /*entrance*/
+
   const _ = jdash = freeSelf._ = freeSelf.jdash = function(selector, context) {
     return new _.fn.init(selector, context);
   };
-
+  
   _.fn = _.prototype;
+  
   _.fn.init = function(selector, context) {
     context = context || document;
+    const self = this;
     try {
-      const nodeList = context.querySelectorAll(selector);
-      this.length = nodeList.length;
-      for (let　i = 0; i < nodeList.length; i++) {
-        this[i] = nodeList[i];
+      let domArr;
+      if (isArray(selector)) {
+        domArr = selector;
+      } else if (isString(selector)) {
+        const nodeList = context.querySelectorAll(selector);
+        domArr = Array.from(nodeList);
       }
+      domArr.forEach(function(ele, idx) {
+        self[idx] = ele;
+      });
+      self.length = domArr.length;
     } catch (e) {
-      const tempDiv = document.createElement('div');
-      tempDiv.innerHTML = selector;
-      this[0] = tempDiv.firstChild;
-      this.length = 1;
+      self[0] = createElement(selector);
+      self.length = 1;
     }
   };
-  _.fn.init.prototype = _.fn;
 
+  _.fn.init.prototype = _.fn;
+  
   _.extend = _.fn.extend = function(source) {
     for (let prop in source) {
       if (source.hasOwnProperty(prop)) {
@@ -28,6 +38,8 @@
       }
     }
   };
+
+  /*tool functions*/
 
   function isObject(arg) {
     return typeof arg === 'object';
@@ -41,31 +53,6 @@
     return typeof arg === 'string';
   }
 
-  _.fn.extend({
-    each(func) {
-      if (func) {
-        for (let i = 0; i < this.length; i++) {
-          func.call(this[i], this[i], i);
-        }
-      }
-      return this;
-    },
-    append(child) {
-      child = child[0] || child;
-      if (!child || !(child instanceof HTMLElement)) {
-        throw new Error('Not a valid element!');
-      }
-      this[0].appendChild(child);
-      return this[0];
-    },
-    on(event, callback) {
-      this.each(function(ele, idx) {
-        ele.addEventListener(event, callback, false);
-      });
-      return this;
-    }
-  });
-
   const isArray = Array.isArray;
 
   function handleClass(jdashObj, className, strategy) {
@@ -75,40 +62,101 @@
       });
     } else if (isString(className)) {
       const newClasses = className.split(/\s/);
-      jdashObj.each(function(ele, idx) {
+      jdashObj.each(function(ele) {
         newClasses.forEach(function(cls) {
           ele.classList[strategy](cls);
         });
       });
     }
   }
+  
+  function createElement(str) {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = str;
+    return wrapper.firstChild;
+  }
+
+  /*proto functions*/
+
+  _.fn.extend({
+    each(func) {
+      if (func && isFunction(func)) {
+        for (let i = 0; i < this.length; i++) {
+          func.call(this[i], this[i], i);
+        }
+      }
+      return this;
+    }
+  });
+
+  /*dom handling functions*/
+
+  _.fn.extend({
+    append(child) {
+      child = child[0] || child;
+      if (!child || !(child instanceof HTMLElement)) {
+        throw new Error('Not a valid element!');
+      }
+      this[0].appendChild(child);
+      return this[0];
+    },
+    appendTo(target) {
+      const self = this;
+      if (isString(target)) {
+        _(target).each(function(parent) {
+          self.each(function(child) {
+            parent.appendChild(child);
+          });
+        });
+      }
+      return this;
+    },
+    remove() {
+      this.each(function(ele, idx) {
+        ele.parentNode.removeChild(ele);
+      });
+    }
+  });
+
+  /*event functions*/
+
+  _.fn.extend({
+    on(event, callback) {
+      this.each(function(ele) {
+        ele.addEventListener(event, callback, false);
+      });
+      return this;
+    }
+  });
+
+  /*style functions*/
 
   _.fn.extend({
     css(propRaw, value) {
       if (isString(propRaw)) {
-        this.each(function(ele, idx) {
+        this.each(function(ele) {
           ele.style[propRaw] = value;
         });
       } else if (isObject(propRaw)) {
-        this.each(function(ele, idx) {
+        this.each(function(ele) {
           for (let prop in propRaw) {
-            this[i].style[prop] = propRaw[prop];
+            ele.style[prop] = propRaw[prop];
           }
         });
       }
       return this;
     },
-    addClass(className) {
-      handleClass(this, className, 'add');
-      return this;
-    },
     hasClass(className) {
-      this.each(function(ele, idx) {
+      this.each(function(ele) {
         if (ele.classList.contains(className)) {
           return true;
         }
       });
       return false;
+    },
+    addClass(className) {
+      handleClass(this, className, 'add');
+      return this;
     },
     removeClass(className) {
       handleClass(this, className, 'remove');
@@ -117,6 +165,16 @@
     toggleClass(className) {
       handleClass(this, className, 'toggle');
       return this;
+    }
+  });
+
+  _.fn.extend({
+    clone() {
+      const newObjs = [];
+      this.each(function(ele) {
+        newObjs.push(createElement(ele.outerHTML));
+      });
+      return new _.fn.init(newObjs);
     }
   });
 
