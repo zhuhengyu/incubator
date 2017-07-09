e9db980545d3decaa93d54aa5a6f1fccdf176594
Ouyang <ch.k.ouyang@gmail.com>
2017-07-09 23:01:19 +0800

    requirejs basic demo

--- demo-requirejs/foo.js
+++ demo-requirejs/foo.js
@@ -1,4 +1,17 @@
 define(function() {
-  const str = 'hello requirejs';
-  alert(str);
+  const currying = (fn, fnLength = fn.length) => {
+    return function() {
+      const args = Array.from(arguments);
+      if (args.length >= fnLength) {
+        return fn.apply(null, args);
+      } else {
+        return currying(function() {
+          return fn.apply(null, args.concat(Array.from(arguments)));
+        }, fnLength - args.length);
+      }
+    }
+  };
+  return {
+    currying
+  };
 });
--- demo-requirejs/main.js
+++ demo-requirejs/main.js
@@ -1,1 +1,7 @@
-require(['foo']);
+require(['foo'], function(foo) {
+	const add = foo.currying(function(a, b) {
+		return a + b;
+	});
+	alert(add(1, 2));
+	alert(add(1)(2));
+});
