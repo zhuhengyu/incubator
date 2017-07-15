445494a5577c9883d12a40cd35f51d0fcbe0fa62
Ouyang <ch.k.ouyang@gmail.com>
2017-07-15 22:12:39 +0800

    demo-react basic demo

--- demo-react/src/components/Counter.jsx
+++ demo-react/src/components/Counter.jsx
@@ -4,24 +4,17 @@ import PropTypes from 'prop-types';
 class Counter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      count: props.initCount
-    };
     // this.plus = ::this.plus
     this.plus = this.plus.bind(this);
     this.minus = this.minus.bind(this);
   }
   plus() {
     // modify state directly can change component's state but will not trigger render method
-    this.state.count++;
-    this.setState({
-      count: this.state.count + 1
-    });
+    // this.state.count++;
+    this.props.update(this.props.index, this.props.initCount + 1);
   }
   minus() {
-    this.setState({
-      count: this.state.count - 1
-    });
+    this.props.update(this.props.index, this.props.initCount - 1);
   }
   render() {
     const style = {
@@ -29,8 +22,8 @@ class Counter extends React.Component {
     };
     return (
       <div>
-        <button onClick={()=>this.minus()}>-</button>&nbsp;&nbsp;&nbsp;&nbsp;
-        <span style={style}>{this.state.count}</span>&nbsp;&nbsp;&nbsp;&nbsp;
+        <button onClick={this.minus}>-</button>&nbsp;&nbsp;&nbsp;&nbsp;
+        <span style={style}>{this.props.initCount}</span>&nbsp;&nbsp;&nbsp;&nbsp;
         <button onClick={this.plus}>+</button>
       </div>
     );
@@ -39,12 +32,15 @@ class Counter extends React.Component {
 
 // propTypes check should be removed in production environment
 Counter.propTypes = {
-  initCount: PropTypes.number
+  index: PropTypes.number.isRequired,
+  initCount: PropTypes.number,
+  update: PropTypes.func,
 };
 
 // defaultProps is very necessary
 Counter.defaultProps = {
-  initCount: 0
+  initCount: 0,
+  update: foo => foo
 };
 
 export default Counter;
--- demo-react/src/components/CounterPanel.jsx
+++ demo-react/src/components/CounterPanel.jsx
@@ -6,14 +6,34 @@ import Counter from './Counter';
 class CounterPanel extends React.Component {
   constructor(props) {
     super(props);
+    const counts = [1, 1, 1];
     this.state = {
-      counterNum: props.counterNum
+      counts
     };
+    this.onUpdate = this.onUpdate.bind(this);
+  }
+  onUpdate(idx, count) {
+    const {
+      counts
+    } = this.state;
+    counts[idx] = count;
+    this.setState({
+      counts
+    });
+  }
+  getSum() {
+    return this.state.counts.reduce((a, b) => a + b);
   }
   render() {
     return (
       <div>
-        <Counter/>
+        <Counter initCount={this.state.counts[0]} update={this.onUpdate} index={0}/>
+        <Counter initCount={this.state.counts[1]} update={this.onUpdate} index={1}/>
+        <Counter initCount={this.state.counts[2]} update={this.onUpdate} index={2}/>
+        <hr/>
+        <span>{this.getSum()}</span>
+        <br/>
+        <button onClick={()=>this.forceUpdate()}>update</button>
       </div>
     );
   }
