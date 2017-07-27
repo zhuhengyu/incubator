0176c987096effcd832c9d2b42e96e5617b270b0
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-07-28 02:03:14 +0800

    demo-electron & react basic demo

---
+++ demo-electron/src/Counter/view.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default Counter;
